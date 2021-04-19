d1584d14383bc55333e13e836b4e159b7250bb83
ahrimaWw <61127756+ahrimaWw@users.noreply.github.com>
2021-04-19 20:51:55 -0300

    Add files via upload

---
+++ amazon/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAgWzuzEWmNdrqyHpNT1AjsbL-eurQ1IvQ",
+    authDomain: "challenge-a2c2c.firebaseapp.com",
+    projectId: "challenge-a2c2c",
+    storageBucket: "challenge-a2c2c.appspot.com",
+    messagingSenderId: "605921361323",
+    appId: "1:605921361323:web:c72ce54a8a435dac7666ee",
+    measurementId: "G-BPXHHFF501"
+  };
